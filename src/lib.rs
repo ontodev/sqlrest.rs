8bb5e98b2e7f90b4edc880e87a0ed00ac90fe561
Michael Cuffaro <consulting@michaelcuffaro.com>
2023-01-12 12:52:48 -0500

    add local_sql_syntax()

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use regex::Regex;
+use sqlx::any::{AnyKind, AnyPool};
+
+/// Given a SQL string, possibly with unbound parameters represented by the placeholder string
+/// sql_param, and given a database pool, if the pool is of type Sqlite, then change the syntax used
+/// for unbound parameters to Sqlite syntax, which uses "?", otherwise use Postgres syntax, which
+/// uses numbered parameters, i.e., $1, $2, ...
+/// Note that SQL_PARAM must be a 'word' (in the regular expression sense)
+pub fn local_sql_syntax(pool: &AnyPool, sql_param: &str, sql: &str) -> String {
+    // The reason that SQL_PARAM must be a word is that below we are matchng against it using '\b'
+    // which represents a word boundary. If you want to use a non-word placeholder then you must
+    // also change '\b' in the regex below to '\B'.
+    //
+    // Do not replace instances of sql_param if they are within quotation marks:
+    let rx = Regex::new(&format!(
+        r#"('[^'\\]*(?:\\.[^'\\]*)*'|"[^"\\]*(?:\\.[^"\\]*)*")|\b{}\b"#,
+        sql_param
+    ))
+    .unwrap();
+
+    let mut final_sql = String::from("");
+    let mut pg_param_idx = 1;
+    let mut saved_start = 0;
+    for m in rx.find_iter(sql) {
+        let this_match = &sql[m.start()..m.end()];
+        final_sql.push_str(&sql[saved_start..m.start()]);
+        if this_match == sql_param {
+            if pool.any_kind() == AnyKind::Postgres {
+                final_sql.push_str(&format!("${}", pg_param_idx));
+                pg_param_idx += 1;
+            } else {
+                final_sql.push_str(&format!("?"));
+            }
+        } else {
+            final_sql.push_str(&format!("{}", this_match));
+        }
+        saved_start = m.start() + this_match.len();
+    }
+    final_sql.push_str(&sql[saved_start..]);
+    final_sql
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: Add an actual unit test here.
+    #[test]
+    fn sqlite_syntax() {
+        assert_eq!(true, true)
+    }
+}
